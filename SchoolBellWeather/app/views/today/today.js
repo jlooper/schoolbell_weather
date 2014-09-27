beeeda06fa6b63c4a6760a271ada914cd76fe80e
jlooper <jen.looper@gmail.com>
2014-09-26 20:56:45 -0400

    final pass and refactor, much more DRY

--- SchoolBellWeather/app/views/today/today.js
+++ SchoolBellWeather/app/views/today/today.js
@@ -24,19 +24,129 @@ define([
 
       navbar = e.view.header.find('.km-navbar').data('kendoMobileNavBar');
 
+      if (localStorage.getItem('transportation') === null){
+        localStorage.setItem('transportation','bus');
+      }
+      $('.transportation').attr("src","img/"+localStorage.getItem("transportation")+".png");
+
+      if (localStorage.getItem('departure_time') === null){
+        localStorage.setItem('departure_time','08:00');
+      }
     },
     afterShow: function (e) {
 
-    //refresh the departure variables
+      var forecast_key = 'c9002942b156fa5d0583934e2b1eced8';
 
-    var todays_date = moment().format('YYYY-MM-DD').toString();
-    var departure_time = todays_date+'T'+localStorage.getItem('departure_time')+':00'
-    var forecast_key = 'c9002942b156fa5d0583934e2b1eced8';
 
-    var lat = localStorage.getItem('lat')
-    var long = localStorage.getItem('long')
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+              function (position) {
+                  localStorage.setItem('lat',position.coords.latitude);
+                  localStorage.setItem('long',position.coords.longitude);
+
+                  var lat = localStorage.getItem('lat');
+                  var long = localStorage.getItem('long');
+
+
+                  
+
+                  getCurrent(lat,long)
+                  
+ 
+              },
+              function (err) {
+                  alert("sorry, we couldn't get your location!")
+                  
+              });
+      }
+      else{
+        alert("sorry, we couldn't get your location!")
+      }
+
+      var getCurrent = function(){
+
+        var todays_date = moment().format('YYYY-MM-DD').toString();
+        var departure_time = todays_date+'T'+localStorage.getItem('departure_time')+':00'
+        
+        var lat = localStorage.getItem('lat')
+        var long = localStorage.getItem('long')
+
+        var url = 'https://api.forecast.io/forecast/' + forecast_key + '/' + lat + ',' + long + '';
+        var forecast = new kendo.data.DataSource({
+
+        transport: {
+              read: {
+                    url: function(options) {
+                        return url
+                        },
+                      dataType: "jsonp"
+                  }
+                },
+                schema: {
+                  data: "daily.data"
+                },
+                requestEnd: function( event ) {
+
+                  
+                    var current_conditions = event.response.currently;
+                  
+                    var tmp = current_conditions.temperature.toString().split('.');                    
+                    
+                    
+                    localStorage.setItem('current_icon','icon-'+current_conditions.icon);
+                    localStorage.setItem('current_temp',tmp[0]+'&deg;');  
+                
+                    $('.current').addClass('icon-'+current_conditions.icon);
+                    $('.current_temp').html(tmp[0]+'&deg;');
+
+                    var current_conditions = event.response.currently;
+                    var forecast = event.response.daily;
+
+                    var tmp = current_conditions.temperature.toString().split('.');                    
+                    
+                    //we do an initial set here
+                    $('.current').addClass('icon-'+current_conditions.icon);
+                    $('.current_temp').html(tmp[0]+'&deg;');
+
+                    localStorage.setItem('current_icon','icon-'+current_conditions.icon);
+                    localStorage.setItem('current_temp',tmp[0]+'&deg;');  
+
+                    $('.summary').html(forecast.summary);
+                    for (i = 0; i < forecast.data.length; i++) { 
+                      $('.date_'+i).html(moment.utc(forecast.data[i].time, 'X').format('dddd'));
+                      $('.day_'+i).html(forecast.data[i].summary);
+                      $('.day_'+i+'_icon').addClass('icon-'+forecast.data[i].icon);
+                      
+                      var minTemp = forecast.data[i].temperatureMin.toString().split('.');
+                      var maxTemp = forecast.data[i].temperatureMax.toString().split('.');
+
+                      $('.day_'+i+'_temps').html('Temperatures between '+minTemp[0]+'&deg; and '+maxTemp[0]+'&deg;');
+                    }
+
+                    getTimed(lat,long)
+
+                    
+                }
+
+
+          });
+
+  forecast.read();
+        
+      }
+   
+  var getTimed = function(lat,long){
 
     //get timed forecast
+ var todays_date = moment().format('YYYY-MM-DD').toString();
+
+ 
+
+    var departure_time = todays_date+'T'+localStorage.getItem('departure_time')+':00'
+
+    console.log(departure_time)
+
+
     var timed_url = 'https://api.forecast.io/forecast/' + forecast_key + '/' + lat + ',' + long + ',' + departure_time + '';
     
     var forecast = new kendo.data.DataSource({
@@ -100,9 +210,9 @@ define([
                       }
                     
 
-                    $('#please_wait').hide();
                     $('#conditions').show();
                     $('.header').show();
+                    $("#please_wait").data("kendoMobileModalView").close();
                    
 
                 }
@@ -111,6 +221,7 @@ define([
           });
 
     forecast.read();
+  }
 
       page = e.view.params.page || 'Today';
       
@@ -120,6 +231,9 @@ define([
         
       $('.transportation').attr("src","img/"+localStorage.getItem("transportation")+".png");
 
+    },
+    closeModal: function (e) {
+      $("#tutorial-modal").data("kendoMobileModalView").close();
     }
   };
 
